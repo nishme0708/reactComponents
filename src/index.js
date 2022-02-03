6693001d939f1e1ad08bfd77235dde15821999e7
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-02-03 16:09:21 +0530

    Add approval card changes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from '@faker-js/faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = function() {
     return (
         <div className='ui container comments'>
-            <CommentDetail author="Nishant" timeAgo="Todat at 4:00 PM" avatar={faker.image.avatar()} text="Cool blog post"/>
-            <CommentDetail author="Darshan" timeAgo="Todat at 5:00 PM" avatar={faker.image.avatar()} text="Nice blog post"/>
-            <CommentDetail author="Swapnil" timeAgo="Todat at 6:00 PM" avatar={faker.image.avatar()} text="Super blog post"/>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Nishant'
+                    timeAgo='Todat at 4:00 PM'
+                    avatar={faker.image.avatar()}
+                    text='Cool blog post'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Darshan'
+                    timeAgo='Todat at 5:00 PM'
+                    avatar={faker.image.avatar()}
+                    text='Nice blog post'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Swapnil'
+                    timeAgo='Todat at 6:00 PM'
+                    avatar={faker.image.avatar()}
+                    text='Super blog post'
+                />
+            </ApprovalCard>
         </div>
     );
 };
