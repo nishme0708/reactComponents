91063d21438f255be0a770c575db1c6c4dd1c566
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-02-03 15:33:07 +0530

    Add comment changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
+import faker from '@faker-js/faker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = function() {
+    return (
+        <div className='ui container comments'>
+            <CommentDetail author="Nishant" timeAgo="Todat at 4:00 PM" avatar={faker.image.avatar()} text="Cool blog post"/>
+            <CommentDetail author="Darshan" timeAgo="Todat at 5:00 PM" avatar={faker.image.avatar()} text="Nice blog post"/>
+            <CommentDetail author="Swapnil" timeAgo="Todat at 6:00 PM" avatar={faker.image.avatar()} text="Super blog post"/>
+        </div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
